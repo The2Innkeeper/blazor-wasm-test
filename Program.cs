e1c3d34343d5730440f46e21e4337a948ba72532
The__Innkeeper <139631318+The2Innkeeper@users.noreply.github.com>
2024-03-03 23:12:07 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using web_ballistics_test;
+using blazor_wasm_test;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
